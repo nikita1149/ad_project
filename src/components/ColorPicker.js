ddcae86f8f7ec5fe02fd0a0200043c6781447670
nikita1149 <nikitaarora917@gmail.com>
2024-04-18 21:25:45 +0530

    last 5 colors displayed

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -45,6 +45,42 @@
 // };
  
 // export default ColorPicker;
+// import React, { useState } from 'react';
+// import { SketchPicker } from 'react-color';
+
+// const ColorPicker = ({ setBackground }) => {
+//   const [sketchPickerColor, setSketchPickerColor] = useState({
+//     r: '3',
+//     g: '105',
+//     b: '161',
+//     a: '1',
+//   });
+
+//   const handleColorChange = (color) => {
+//     setSketchPickerColor(color.rgb);
+//     setBackground(`rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`);
+//   };
+
+//   return (
+//     <div className="App" style={{ display: 'flex', justifyContent: 'space-around' }}>
+//       <div className="sketchpicker">
+//         <h6>Sketch Picker</h6>
+//         <div
+//           style={{
+//             backgroundColor: `rgba(${sketchPickerColor.r},${sketchPickerColor.g},${sketchPickerColor.b},${sketchPickerColor.a})`,
+//             width: 100,
+//             height: 50,
+//             border: '2px solid white',
+//           }}
+//         ></div>
+//         <SketchPicker onChange={handleColorChange} color={sketchPickerColor} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ColorPicker;
+
 import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
 
@@ -55,10 +91,24 @@ const ColorPicker = ({ setBackground }) => {
     b: '161',
     a: '1',
   });
+  const [selectedColors, setSelectedColors] = useState([]);
+  const [showPicker, setShowPicker] = useState(false);
 
   const handleColorChange = (color) => {
+    // Update the selected color
     setSketchPickerColor(color.rgb);
     setBackground(`rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`);
+
+    // Add the selected color to the list of selected colors
+    setSelectedColors(prevColors => {
+      // Limit the list to store only the last 5 selected colors
+      const updatedColors = [{ ...color.rgb }, ...prevColors.slice(0, 4)];
+      return updatedColors;
+    });
+  };
+
+  const togglePicker = () => {
+    setShowPicker(!showPicker);
   };
 
   return (
@@ -72,12 +122,41 @@ const ColorPicker = ({ setBackground }) => {
             height: 50,
             border: '2px solid white',
           }}
+          onClick={togglePicker}
         ></div>
-        <SketchPicker onChange={handleColorChange} color={sketchPickerColor} />
+        {showPicker && <SketchPicker onChange={handleColorChange} color={sketchPickerColor} />}
+      </div>
+      <div>
+        <h6>Last 5 Selected Colors:</h6>
+        <div style={{ display: 'flex' }}>
+          {selectedColors.map((color, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundColor: `rgba(${color.r},${color.g},${color.b},${color.a})`,
+                width: 30,
+                height: 30,
+                margin: '5px',
+                cursor: 'pointer',
+              }}
+              onClick={() => setSketchPickerColor(color)}
+            ></div>
+          ))}
+          <button onClick={togglePicker} style={{ marginLeft: '5px' }}>+</button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ColorPicker;
+
+
+
+
+
+
+
+
+
 
